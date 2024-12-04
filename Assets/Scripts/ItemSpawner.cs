d0dcfd570798c948f5ac9accd5b5b236e1673092
Guilherme Padovani Bensone <guibensone@gmail.com>
2024-12-04 19:23:19 -0300

    Whitelabel

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.U2D.Aseprite;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
